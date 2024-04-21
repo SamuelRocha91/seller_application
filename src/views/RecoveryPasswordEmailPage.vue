104b2bd7172a7ee97b2f1ea8d088b88ae7224a15
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-21 13:05:11 -0300

    feat: cria view de página após o usuário receber um link

---
+++ src/views/RecoveryPasswordEmailPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ImageContainer from '@/components/access/ImageContainer.vue'
+import Bruschetta from '../assets/imageContainer/Bruschetta.jpg'
+import HeaderAccess from '@/components/access/HeaderAccess.vue'
+import FormRecoveryPasswordEmail from '@/components/forms/FormRecoveryPasswordEmail.vue'
+</script>
+
+<template>
+  <div class="login-frame">
+    <ImageContainer :src="Bruschetta" alt="pão, azeite e alho" />
+    <main>
+      <HeaderAccess />
+      <FormRecoveryPasswordEmail />
+    </main>
+  </div>
+</template>
