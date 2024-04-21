50fd78dbaba5ef604ff54e40a87953499304395e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-20 22:17:57 -0300

    feat: cria view de página de registro

---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HeaderAccess from '../components/access/HeaderAccess.vue';
+import ImageContainer from '../components/access/ImageContainer.vue';
+import Platter from '../assets/imageContainer/Platter.jpg'
+</script>
+
+<template>
+  <div class="login-frame">
+    <ImageContainer :src="Platter" alt="Imagem de um prato de sushi" />
+    <main>
+      <HeaderAccess />
+      <FormRegister />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.login-frame {
+  display: flex;
+  height: 100vh;
+  box-sizing: border-box;
+  border: 0px;
+  margin: 0px;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  width: 50vw;
+  height: 100%;
+  margin: auto;
+}
+</style>
