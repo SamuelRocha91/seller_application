cd548b2631c3a6530132043dd22dbe1d9c9a27a8
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-21 12:02:03 -0300

    feat: inicia criação da view de RecoveryPasswordPage

---
+++ src/views/RecoveryPasswordPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeaderAccess from '../components/access/HeaderAccess.vue';
+import ImageContainer from '../components/access/ImageContainer.vue';
+import Hamburguer from '../assets/imageContainer/Hamburger.jpg'
+</script>
+
+<template>
+  <div class="login-frame">
+    <ImageContainer :src="Hamburguer" alt="Imagem de um hambúrguer com fritas" />
+    <main>
+      <HeaderAccess />
+    </main>
+  </div>
+</template>
