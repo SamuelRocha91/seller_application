0d7cd9e9fbd9deca1a5bb4eb76ed4addcc8426a1
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-20 21:47:03 -0300

    feat: implementa css da página de login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
 import ImageContainer from '../components/access/ImageContainer.vue'
-import HeaderAccess from '@/components/access/HeaderAccess.vue';
-import FormLogin from '@/components/forms/FormLogin.vue';
+import HeaderAccess from '@/components/access/HeaderAccess.vue'
+import FormLogin from '@/components/forms/FormLogin.vue'
 import Vegetais from '../assets/imageContainer/Vegetables.png'
 </script>
 
 <template>
   <div class="login-frame">
-    <ImageContainer :src="Vegetais"  alt="Imagem de uma cesta de vegetais" />
+    <ImageContainer :src="Vegetais" alt="Imagem de uma cesta de vegetais" />
     <main>
       <HeaderAccess />
       <FormLogin />
     </main>
   </div>
 </template>
+
+<style scoped>
+.login-frame {
+  display: flex;
+  height: 100vh;
+  box-sizing: border-box;
+  border: 0px;
+  margin: 0px;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  width: 50vw;
+  height: 100%;
+  margin: auto;
+}
+</style>
