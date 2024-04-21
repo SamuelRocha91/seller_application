6ccf6f25d3a1295e52204d6eaccb4f46b9500dc3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-21 12:57:26 -0300

    feat: cria no arquivo de rotas uma rota simulada pra componente de recuperação de senha enviado por email

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import HomePage from '../views/HomePage.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import RegisterPage from '@/views/RegisterPage.vue'
 import RecoveryPasswordPage from '@/views/RecoveryPasswordPage.vue'
+import RecoveryPasswordEmailPage from '@/views/RecoveryPasswordEmailPage.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +28,11 @@ const router = createRouter({
       path: '/recuperar-senha',
       name: 'recuperar-senha',
       component: RecoveryPasswordPage
+    },
+    {
+      path: '/recuperar-senha-link',
+      name: 'recuperar-senha-link',
+      component: RecoveryPasswordEmailPage
     },
   ]
 })
