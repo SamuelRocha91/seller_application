d03ebd4b8190d47b7e7313e94fe8cbec4b24ebad
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-21 19:57:07 -0300

    feat: cria navBar small em caso de clique no painel

---
+++ src/components/dashboard/NavBarSmall.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <nav class="nav-links">
+    <ul>
+      <li class="links-button">
+        <img class="image-dimension" src="../../assets//navBar/Home.png" alt="Ícone de home" />
+        <RouterLink class="links-style" to="/dashboard">Início</RouterLink></li>
+      <li class="links-button">
+        <img class="image-dimension" src="../../assets//navBar/Order.png" alt="ícone de pedidos" />
+        <RouterLink class="links-style"  to="/dashboard/pedidos">Pedidos</RouterLink></li>
+      <li class="links-button">
+        <img class="image-dimension" src="../../assets/navBar/Store.png" alt="ícone de loja" />
+        <RouterLink class="links-style"  to="/dashboard/loja">Gerenciar loja</RouterLink></li>
+      <li class="links-button">
+        <img src="../../assets/navBar/Reports.png"  alt="ícone de relatório" />
+        <RouterLink class="links-style" to="/dashboard/relatorios">Relatórios</RouterLink>
+      </li>
+      <li class="links-button">
+        <img src="../../assets/navBar/Finance.png" alt="ícone de finanças" />
+        <RouterLink class="links-style"  to="/dashboard/finacas">Finanças</RouterLink></li>
+      <li class="links-button">
+        <img class="image-dimension" src="../../assets/navBar/Settings.png" alt="Ícone de configurações">
+        <RouterLink class="links-style"  to="/configuracoes">Configurações</RouterLink></li>
+      <li class="links-button">
+        <img src="../../assets/navBar/Logout.png" alt="Ícone de sair">
+        <RouterLink class="links-style"  to="/">Sair</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+<style scoped>
+.nav-links {
+  display: flex;
+  border: 1px solid black;
+  height: 100%;
+  display: flex;
+  background-color: #FFF8F8;
+}
+
+ul {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+ul li {
+  height: 14.2857%;
+  width: 100%;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  gap: 10px;
+  padding: 20px;
+  font-size:small ;
+}
+
+img {
+  color: black;
+  image-resolution: inherit;
+}
+
+.img {
+  height: 20px;
+  width: 20px;
+}
+
+.links-style {
+  text-decoration: none;
+  color: black;
+}
+
+.links-button:hover {
+  background-color: rgb(185, 250, 228);
+  width: 100%;
+}
+
+.links-style:hover {
+  color: red;
+}
+</style>
