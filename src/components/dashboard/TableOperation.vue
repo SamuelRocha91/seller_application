27458dd3a04a6bd11145c6a9e2cb3c50ca52747f
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-09 10:38:15 -0300

    feat: cria tabela pra renderizar horários de funcionamento

---
+++ src/components/dashboard/TableOperation.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+type operation = {
+  open: string,
+  closed: string,
+  day: string
+}
+
+const { data } = defineProps<{
+  data: Array<operation>
+}>()
+
+</script>
+<template>
+  <table>
+    <caption>
+      Agenda semanal de funcionamento do estabeleciomento
+    </caption>
+    <thead>
+      <tr>
+        <th scope="col">Dia</th>
+        <th scope="col">Horário de abertura</th>
+        <th scope="col">Horário de fechamento</th>
+        <th scope="col">Excluir agenda</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(operation, index) in data" :key="index">
+        <td>{{ operation.day }}</td>
+        <td>{{ operation.open }}</td>
+        <td>{{ operation.closed }}</td>
+        <td class="btn-delete"><button>X</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+}
+
+caption {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+thead th {
+  background-color: #f2f2f2;
+  color: #333;
+  padding: 10px;
+  text-align: left;
+}
+
+tbody td {
+  border-bottom: 1px solid #ddd;
+  padding: 10px;
+}
+
+tbody tr:last-child td {
+  border-bottom: none;
+}
+
+button {
+  background-color: #dc3545;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+
+button:hover {
+  background-color: #c82333;
+}
+
+</style>
