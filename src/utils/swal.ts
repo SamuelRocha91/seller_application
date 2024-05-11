dbf512c430a7665fa666bfb8c798b8464c873ad2
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-11 20:49:50 -0300

    refactor: centraliza lógica de swall em arquivo de utilidades

---
+++ src/utils/swal.ts
@@ -0,0 +1,47 @@
+import Swal from "sweetalert2";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const swalError = (title: string, text: string) => {
+  Swal.fire({
+    title: `${title}`,
+    text: `${text}`,
+    icon: 'error',
+    confirmButtonText: 'Ok'
+  });
+};
+
+const swalSuccess = (text: string) => {
+  Swal.fire(`${text}`);
+};
+
+const swalWithRedirect = () => {
+  let timerInterval: NodeJS.Timeout | undefined;
+  Swal.fire({
+    title: 'Cadastro efetuado com sucesso!',
+    text: 'Você será redirecionado para a página de login.',
+    icon: 'success',
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const timer = Swal.getPopup()?.querySelector('b');
+      if (timer) {
+        timerInterval = setInterval(() => {
+          timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+      }
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    }
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log('I was closed by the timer');
+      router.push('/login');
+    }
+  });
+};
+
+export { swalError, swalSuccess, swalWithRedirect};
