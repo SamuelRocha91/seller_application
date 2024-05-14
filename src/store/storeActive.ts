81c45eae4e858ba1db0057571ac935f0ddf77e9b
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-14 16:28:02 -0300

    cria configuração de estado global pinia de loja do usuário ativa

---
+++ src/store/storeActive.ts
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useStoreActive = defineStore('store', () => {
+  const storeActive = ref({
+    active: false,
+    address: '', 
+    description: '',
+    id: 0,
+    phoneNumber: '',
+    price: '',
+    src: '',
+    category: ''
+  });
+  function setStore(data: any) {
+    storeActive.value.active = data.active;
+    storeActive.value.address = data.address;
+    storeActive.value.description = data.description;
+    storeActive.value.id = data.id;
+    storeActive.value.phoneNumber = data.phoneNumber;
+    storeActive.value.price = data.price;
+    storeActive.value.src = data.src;
+    storeActive.value.category = data.category;
+  }
+
+  return { setStore, storeActive };
+});
