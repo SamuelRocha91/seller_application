0f0e1f101239012c94a623eac437729d474f3912
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-15 15:41:21 -0300

    feat: insere parâmetro na swall com redirecionamento e cria swal para deleção

--- src/utils/swal.ts
+++ src/utils/swal.ts
@@ -1,6 +1,5 @@
 import Swal from "sweetalert2";
 
-
 const swalError = (title: string, text: string) => {
   Swal.fire({
     title: `${title}`,
@@ -14,7 +13,7 @@ const swalSuccess = (text: string) => {
   Swal.fire(`${text}`);
 };
 
-const swalWithRedirect = () => {
+const swalWithRedirect = (action: () => void) => {
   // @ts-ignore
   let timerInterval: NodeJS.Timeout | undefined;
   Swal.fire({
@@ -38,8 +37,25 @@ const swalWithRedirect = () => {
   }).then((result) => {
     if (result.dismiss === Swal.DismissReason.timer) {
       console.log('I was closed by the timer');
+      action();
     }
   });
 };
 
-export { swalError, swalSuccess, swalWithRedirect};
+const swallWithDelete = (handleDelete: () => void) => {
+  Swal.fire({
+    title: "Deseja continuar?",
+    text: "Deletar essa loja acarretará numa exclusão irreversível",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Sim, delete ela pai!",
+    cancelButtonText: "Não!! Tá doido uai???"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      handleDelete();
+    }
+  });
+};
+export { swalError, swalSuccess, swalWithRedirect, swallWithDelete};
