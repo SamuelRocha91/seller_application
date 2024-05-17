0e110f561e60a971327c31fd89263d0d6b1f02ae
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-17 19:07:03 -0300

    feat: inicia implementação de classe abstrata de base da service
    cria um atributo protegido de api e um de acesso ao storage instanciados no construtor

---
+++ src/api/abstractService.ts
@@ -0,0 +1,23 @@
+import { createStorage, type SimpleStorage } from '../utils/storage';
+
+abstract class BaseService {
+  protected apiUrl: string;
+  storage: SimpleStorage;
+
+  constructor() {
+    this.apiUrl = import.meta.env.VITE_BASE_URL;
+    const persistent: boolean = this.whatIsMyStorage();
+    this.storage = createStorage(persistent);
+  }
+    
+  private whatIsMyStorage() {
+    const transient = createStorage(false);
+    if (transient.get('token') != undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+export { BaseService };
