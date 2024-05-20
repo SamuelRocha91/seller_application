7760584111498f49ffea7c163d4ab6a12fd0fbf0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-19 21:16:35 -0300

    feat: cria lógica de filtros de produtos

--- src/components/forms/FormRegisterStore.vue
+++ src/components/forms/FormRegisterStore.vue
@@ -7,7 +7,6 @@ import { phoneMask } from '@/utils/formUtils';
 import TableList from '../dashboard/TableList.vue';
 import { type storeType } from '@/types/storeType';
 import { useStoreActive } from '@/store/storeActive';
-import '../../assets/global.css';
 
 let image: File | string;
 
@@ -328,31 +327,291 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.filters-menu label {
+  width: 50%;
+}
+.bg-input-2 {
+  border: 1px solid #dedede;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+  padding: 13px;
+  width: 100%;
+  height: 37px;
+}
+.select-box {
+  width: 200px;
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+}
+.select-box-2 {
+  width: 400px;
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+  height: 41px;
+}
+.main-content{
+  background-color: gray;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  gap: 8px;
+  flex-direction: column;
+}
+.custom-file-upload {
+  cursor: pointer;
+  color: #0000ff;
+  text-decoration: underline;
+  padding: 10px 20px;
+  border-radius: 5px;
+}
+form {
+  width: 100%;
+  padding: 10px;
+}
+.form-init {
+  background-color: white;
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  padding: 10px;
+  width: 100%;
+  gap: 10px;
+  border: 1px solid rgb(189, 187, 187);
+}
+.form-container {
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+  align-items: center;
+  padding: 20px;
+  width: 90%;
+  height: 62vh;
+}
+.image-container {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #ccc;
+}
+#uploadedImage {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+.form-init {
+  display: flex;
+}
+.save-form-btn {
+  color: #ffffff;
+  background-color: #14bb1d;
+  font-size: 16px;
+  width: 70px;
+  height: 50px;
+  border-radius: 5px;
+}
+.label-intermediate {
+  margin-left: 4%;
+  width: 90%;
+}
+.save-form-btn:hover {
+  cursor: pointer;
+}
+.filters-menu {
+  margin-top: 5px;
+  background-color: white;  
+  width: 90%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 5px;
+  border-radius: 5px;
+}
+.content-menu {
+  background-color: white;
+  padding: 20px;
+  width: 90%;
+  height: 100%;
+}
+.bg-input {
+  border: 1px solid #dedede;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+  padding: 13px;
+  width: 100%;
+  height: 37px;
+}
+.category {
+  width: 99%;
+}
+.inputs-init {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+.intermediate {
+  width: 100%;
+}
 .image-form {
   display: flex;
   width: 30%;
   flex-direction: column;
-  justify-content: center;
+  justify-content: center;  
   align-items: center;
 }
-
-.intermediate-content {
-  justify-content: space-around;
-}
-
 .section-intermediate {
-  justify-content: center;
+  display: flex;
+  padding: 10px;
+  width: 100%;
+  padding: 10px;
+  justify-content: space-between;
+  border-right: 1px solid rgb(189, 187, 187);
+  border-left: 1px solid rgb(189, 187, 187);
+  align-items: center;
 }
-
+.intermediate-content {
+  display: flex;
+  width: 100%;
+  gap: 40px;
+  padding: 10px;
+  justify-content: space-between;
+}
 .section-finish {
-  height: 32.5vh;
+  width: 100%;
+  height: 32.0vh;
+  display: flex;
+  justify-content: center;
+  border: 1px solid #ccc;
 }
-
 .text-description {
   display: flex;
   flex-direction: column;
-  width: 82%;
   justify-content: center;
+  padding: 20px;
+  width: 87%;
+  height: fit-content;
+  justify-content: center;
+}
+textarea {
+  width: 90%;
+  height: 100%;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+  padding: 15px;
+}
+.btn-div {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 100%;
+}
+.input-phone {
+  width: 30%;
+}
+h2 {
+  font-weight: bold;
+  font-size: 20px;
+}
+.inputs-two {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+}
+.input-simulate {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  outline: none;
+  padding: 13px;
+  width: 100%;
+  height: 37px;
+}
+.section-finish-two {
+  width: 100%;
+  height: 35vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  padding: 30px;
+  margin-left: 21px;
+  gap: 10px;
+}
+.section-finish-two .text-description {
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 10px;
+}
+.text-description .input-simulate {
+  height: 80%;
+}
+.address {
+  width: 100%;
+}
+.address .input-simulate {
+  width: 91%;
+}
+.name-store {
+  width: 100%;
+}
+.name-store .input-simulate {
+  width: 91%;
+}
+.inputs-two .name {
+  width: 50%;
+}
+.edit-form-btn {
+  color: #ffffff;
+  background-color: #3f07c0;
+  font-size: 16px;
+  width: 70px;
+  height: 50px;
+  border-radius: 5px;
+}
+.delete-form-btn {
+  color: #ffffff;
+  background-color: #ff1818;
+  font-size: 16px;
+  width: 70px;
+  height: 50px;
+  border-radius: 5px;
+}
+.register-form-btn {
+  color: #ffffff;
+  background-color: #ff1818;
+  font-size: 16px;
+  width: 230px;
+  height: 50px;
+  border-radius: 5px;
+}
+.edit-form-btn:hover,
+.delete-form-btn:hover {
+  cursor: pointer;
+}
+.btn-div-two {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 }
 
 </style>
