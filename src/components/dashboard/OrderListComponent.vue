4ceeefe7d87e35696cd108bab06302631f560770
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 10:47:39 -0300

    feat: cria componente que servirá para renderizar o modelo de sintese do pedido

---
+++ src/components/dashboard/OrderListComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="list-group">
+    <div class="list-group-item">
+      <h5 class="mb-1">Nome do Pedido</h5>
+      <p class="mb-1">Status: Pendente</p>
+    </div>
+  </div>
+</template>
