b5934b514ba643007420d2d21174a742029b38ee
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 10:47:11 -0300

    feat: cria componente que servirá de base pra renderizar o pedido na integra

---
+++ src/components/dashboard/OrderContent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h1>Pedido #1</h1>
+    </div>
+    <div class="card-body">
+      <p>Status: Y</p>
+      <p>Cliente: Samuel</p>
+      <p>Cep: 40415-345</p>
+      <p>Endereço: Rua 1, 123</p>
+      <p>Produto: Notebook</p>
+      <p>Quantidade: 1</p>
+      <p>Preço: R$ 3.000,00</p>
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+      <button class="btn btn-success me-2">Confirmar</button>
+      <button class="btn btn-danger">Cancelar</button>
+    </div>
+  </div>
+</template>
