8f59455d69836fc6fe47cbdcaa79d3f093955b87
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 10:46:45 -0300

    feat: cria componente pra listar pedidos e filtros

---
+++ src/components/dashboard/OrderColumn.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import OrderListComponent from './OrderListComponent.vue';
+import FiltersComponent from './FiltersComponent.vue';
+</script>
+<template>
+  <div class="card">
+    <div class="card-body">
+      <FiltersComponent />
+    </div>
+    <div class="card-body">
+      <OrderListComponent />
+    </div>
+  </div>
+</template>
