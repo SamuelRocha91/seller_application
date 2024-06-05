aedcb6c316e2847348271867ef031023287c8323
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 10:46:01 -0300

    feat: cria filtro pra componente de order

---
+++ src/components/dashboard/FiltersComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="mb-3">
+    <input type="text" class="form-control mb-2" placeholder="Buscar pedidos">
+    <select class="form-select">
+      <option value="">Filtrar por status</option>
+      <option value="pendente">Pendente</option>
+      <option value="confirmado">Confirmado</option>
+      <option value="cancelado">Cancelado</option>
+    </select>
+  </div>
+</template>
