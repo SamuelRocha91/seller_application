b0cddeb6d07561b2298d333b0051d92cbd85eeff
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-08 23:07:09 -0300

    feat: estiliza parte de pedidos

--- src/components/dashboard/OrderContent.vue
+++ src/components/dashboard/OrderContent.vue
@@ -4,20 +4,23 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="card" >
+  <div class="card">
     <div class="card-header">
       <h1>Pedido {{ order.id }}</h1>
     </div>
     <div class="card-body">
       <p>Status: {{ order.status }}</p>
-      <div v-for="(data, index) in order.items" :key="index">
-        <p>Produto: {{ data.product }}</p>
-        <p>Quantidade: {{ data.amount }}</p>
-        <p>Preço:{{ data.price }}</p>
+      <div v-for="(data, index) in order.items" :key="index" class="order-item">
+        <div class="product-info">
+          <p>Produto: {{ data.product }}</p>
+          <p>Quantidade: {{ data.amount }}</p>
+          <p>Preço: {{ data.price }}</p>
+        </div>
+        <hr class="divider">
       </div>
     </div>
-    <div>
-      <p>Total: {{ order.total }}</p>
+    <div class="total-section">
+      <p class="total" >Total: R${{ order.total }}</p>
     </div>
     <div v-if="order.status == 'payment_confirmed'" class="card-footer d-flex justify-content-end">
       <button class="btn btn-success me-2">Aceitar</button>
@@ -27,9 +30,38 @@ defineProps<{
       <button class="btn btn-success me-2">Começar preparo</button>
       <button class="btn btn-danger">Cancelar</button>
     </div>
-     <div v-if="order.status == 'in_progress'" class="card-footer d-flex justify-content-end">
+    <div v-if="order.status == 'in_progress'" class="card-footer d-flex justify-content-end">
       <button class="btn btn-success me-2">Pronto para entrega</button>
       <button class="btn btn-danger">Cancelar</button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.order-item {
+  margin-bottom: 20px;
+}
+
+.product-info {
+  margin-bottom: 10px;
+}
+
+.divider {
+  border-top: 1px solid #ccc;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.total-section {
+  margin-top: 20px;
+  border-top: 1px solid #ccc;
+  padding-top: 10px;
+}
+
+.total {
+  color: green;
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 15px;
+}
+</style>
