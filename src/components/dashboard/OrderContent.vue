d50fb0355965e92a0a5ad9465a4c868184d1feb4
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-08 22:20:26 -0300

    feat: ajusta componente de renderização de itens pra detalhar pedido selecionado

--- src/components/dashboard/OrderContent.vue
+++ src/components/dashboard/OrderContent.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
+defineProps<{
+  order: any,
+}>();
 </script>
 <template>
-  <div class="card">
+  <div class="card" >
     <div class="card-header">
-      <h1>Pedido #1</h1>
+      <h1>Pedido {{ order.id }}</h1>
     </div>
     <div class="card-body">
-      <p>Status: Y</p>
-      <p>Cliente: Samuel</p>
-      <p>Cep: 40415-345</p>
-      <p>Endereço: Rua 1, 123</p>
-      <p>Produto: Notebook</p>
-      <p>Quantidade: 1</p>
-      <p>Preço: R$ 3.000,00</p>
+      <p>Status: {{ order.status }}</p>
+      <div v-for="(data, index) in order.items" :key="index">
+        <p>Produto: {{ data.product }}</p>
+        <p>Quantidade: {{ data.amount }}</p>
+        <p>Preço:{{ data.price }}</p>
+      </div>
     </div>
-    <div class="card-footer d-flex justify-content-end">
-      <button class="btn btn-success me-2">Confirmar</button>
+    <div>
+      <p>Total: {{ order.total }}</p>
+    </div>
+    <div v-if="order.status == 'payment_confirmed'" class="card-footer d-flex justify-content-end">
+      <button class="btn btn-success me-2">Aceitar</button>
+      <button class="btn btn-danger">Recusar</button>
+    </div>
+    <div v-if="order.status == 'accepted'" class="card-footer d-flex justify-content-end">
+      <button class="btn btn-success me-2">Começar preparo</button>
+      <button class="btn btn-danger">Cancelar</button>
+    </div>
+     <div v-if="order.status == 'in_progress'" class="card-footer d-flex justify-content-end">
+      <button class="btn btn-success me-2">Pronto para entrega</button>
       <button class="btn btn-danger">Cancelar</button>
     </div>
   </div>
