cfe6cd142162cb93e624e6b6db7878be29b84adc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-19 12:23:32 -0300

    feat: instala chartjs

---
+++ src/components/dashboard/ReportsComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+    <div>
+        <h1>Análise dos últimos 30 dias</h1>
+    </div>
+    <div>
+        <h2>Relatório de saída de produtos por dia da semana</h2>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Média de faturamento por dia da semana</h2>
+    </div>
+</template>
