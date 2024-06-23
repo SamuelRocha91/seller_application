c24230320ca5bf9aa9abb2629be7d0e9eac63419
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-23 14:03:23 -0300

    feat: cria paginação para página de histórico de pedidos

--- src/views/HistoryOrderPage.vue
+++ src/views/HistoryOrderPage.vue
@@ -15,6 +15,10 @@ const date = defineModel('date', { default: '' });
 const BignavBar = ref(true);
 const orders = ref<any[]>([]);
 const store = useStoreActive().storeActive;
+const current = ref(0);
+const next = ref(0);
+const previous = ref(0);
+const pages = ref(0);
 
 const filteredOrders = () => {
   getOrders(1, date.value, category.value.toLowerCase());
@@ -26,7 +30,17 @@ const getOrders = (page: number, date: string, category: string) => {
   OrderService.getOrders(store.id, (data: any) => {
     console.log(data);
     orders.value = data.result.orders;
+    next.value = data.result.pagination.next || 1;
+    pages.value = data.result.pagination.pages;
+    current.value = data.result.pagination.current || 1;
+    previous.value = data.result.pagination.previous || 1;
   }, page, date, category, );
+};
+
+const handlePage = (page: number) => {
+  if (page > 0 && page <= pages.value) {
+    getOrders(page, date.value, category.value.toLowerCase());
+  }
 };
 
 const handleClick = () => {
@@ -82,6 +96,25 @@ onMounted(() => {
         v-model:category="category"
         v-model:date="date"
         />
+        <nav>
+        <ul class="pagination justify-content-end">
+          <li class="page-item" :class="{ disabled: current === 1 }">
+            <a class="page-link" href="#" @click.prevent="handlePage(previous)">
+              Anterior
+            </a>
+          </li>
+          <li class="page-item" v-for="page in pages" :key="page" :class="{ active: current === page }">
+            <a class="page-link" href="#" @click.prevent="handlePage(page)">
+              {{ page }}
+            </a>
+          </li>
+          <li class="page-item" :class="{ disabled: current === pages }">
+             <a class="page-link" href="#" @click.prevent="handlePage(next)">
+               Próxima
+             </a>
+          </li>
+        </ul>
+      </nav>
       </main>
     </div>
   </div>
