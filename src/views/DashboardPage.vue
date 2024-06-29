aebb3793695fe7581ecbf96b4706c0bc955b11e0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-29 20:25:38 -0300

    feat: insere pageinfo de home no dasgboardPage

---
+++ src/views/DashboardPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Home from '../assets/icons/Home.png';
+import PageInfo from '@/components/dashboard/PageInfo.vue';
+</script>
+<template>
+    <div class="dashboard">
+        <PageInfo
+        :src="Home"
+        alt="Home"
+        title="Bem vindo ao Dashboard"
+        description="Visão geral de sua loja"
+        />
+    </div>
+</template>
