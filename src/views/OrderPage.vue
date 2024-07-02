77b4683df2ca752bba09e438d81dab72cbbc2598
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-07-01 23:56:30 -0300

    feat: refatora página de pedidos

--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import NavBar from '../components/dashboard/NavBar.vue';
 import OrderColumn from '../components/dashboard/OrderColumn.vue';
 import OrderContent from '../components/dashboard/OrderContent.vue';
-import NavBarSmall from '../components/dashboard/NavBarSmall.vue'; 
-import HeaderDashboard from '../components/dashboard/HeaderDashboard.vue';
 import { onMounted } from 'vue';
 import orderService from '@/api/orderService';
 import { useStoreActive } from '@/store/storeActive';
-import LoadingSpiner from '@/components/dashboard/LoadingSpiner.vue';
-import { useNavStore } from '@/store/navStore';
 import ChatComponent from '@/components/dashboard/ChatComponent.vue';
 
-const navStore = useNavStore();
 const hasNewOrder = ref(false);
 const isLoading = ref(false);
 const newOrder = ref([]);
 const orderSelected = ref<any>(null);
 const storeActive = useStoreActive().storeActive;
 
-const handleClick = () => {
-  navStore.setNav();
-};
+
 const selectOrder = (id: number) => {
   orderService.getOrderById(id, (data: any) => {
     orderSelected.value = data;
@@ -92,117 +84,32 @@ onMounted(() => {
 </script>
 
 <template>
-  <template v-if="isLoading">
-    <LoadingSpiner :isLoading="isLoading" />
-  </template>
-  <template v-else>
-    <div class="d-flex color-back">
-      <template v-if="navStore.navActive">
-        <div class="header-nav bg-danger d-flex flex-column align-items-center">
-          <div class="filter py-3">
-            <img
-              class="toggle"
-              @click="handleClick"
-              src="../assets/navBar/ToggleNav.png"
-              alt="Botão para reduzir navbar"
-            />
-          </div>
-          <NavBar />
+  <main class="p-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4 w-100 details-order" style="max-height: 85vh; overflow-y: auto;">
+          <OrderColumn v-if="hasNewOrder" :orders="newOrder" :handleSelect="selectOrder"/>
         </div>
-      </template>
-      <template v-else>
-        <div class="header-nav-small bg-danger d-flex flex-column align-items-center">
-          <div class="filter-small py-3">
-            <img
-              @click="handleClick"
-              src="../assets/navBar/ToggleNav.png"
-              alt="Botão para aumentar navbar"
-            />
+        <div class="col-md-8 w-100 details-order">
+          <OrderContent
+            v-if="orderSelected"
+            :order="orderSelected"
+            :acceptOrder="acceptOrder"
+            :cancelOrder="cancelOrder"
+            :startPreparation="startPreparation"
+            :readyForDelivery="readyForDelivery"
+          />
+          <ChatComponent class="chat-component" v-if="orderSelected" :orderId="orderSelected.id"/>
+          <div v-else class="d-flex align-items-center justify-content-center not-content">
+            <h2>Você ainda não selecionou nenhum pedido</h2>
           </div>
-          <NavBarSmall />
         </div>
-      </template>
-      <div class="page-content flex-grow-1">
-        <div class="header-content">
-          <HeaderDashboard />
-        </div>
-        <main class="p-3">
-          <div class="container-fluid">
-            <div class="row">
-             <div class="col-md-4 w-100 details-order" style="max-height: 85vh; overflow-y: auto;">
-               <OrderColumn v-if="hasNewOrder" :orders="newOrder" :handleSelect="selectOrder"/>
-             </div>
-              <div class="col-md-8 w-100 details-order">
-                <OrderContent
-                 v-if="orderSelected"
-                 :order="orderSelected"
-                 :acceptOrder="acceptOrder"
-                 :cancelOrder="cancelOrder"
-                 :startPreparation="startPreparation"
-                 :readyForDelivery="readyForDelivery"
-                 />
-              <ChatComponent class="chat-component" v-if="orderSelected" :orderId="orderSelected.id"/>
-              <div v-else class="d-flex align-items-center justify-content-center not-content">
-                <h2>Você ainda não selecionou nenhum pedido</h2>
-              </div>
-              </div>
-            </div>
-          </div>
-        </main>
       </div>
     </div>
-  </template>
+  </main>    
 </template>
 
 <style scoped>
-.details-order {
-  background-color: gray;
-}
-.color-back {
-  background-color: gray;
-  height: 100vh;
-}
-.header-nav, .header-nav-small {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  z-index: 1000;
-  transition: width 0.5s ease;
-}
-
-.header-nav {
-  width: 15vw;
-}
-
-.header-nav-small {
-  width: 7vw;
-}
-
-.not-content {
- background-color: white;
- height: 500px;
-}
-
-.filter img, .filter-small img {
-  height: auto;
-  max-width: 100%;
-}
-
-.page-content {
-  margin-left: 15vw;
-  transition: margin-left 0.3s ease;
-  background-color: gray;
-}
-
-.header-nav-small ~ .page-content {
-  margin-left: 7vw;
-}
-
-.header-content {
-  height: 17%;
-  background-color: white;
-}
 
 main {
   height: 100vh;
@@ -217,48 +124,5 @@ main {
   max-width: 20%;
   box-shadow: 0px 0px 10px 0px #000000;
 }
-@media (max-width: 860px) {
-  .header-nav, .header-nav-small {
-    width: 30vw;
-  }
-  .header-nav-small {
-    width: 15vw;
-  }
-  .page-content {
-    margin-left: 30vw;
-  }
-  .header-nav-small ~ .page-content {
-    margin-left: 15vw;
-  }
-}
 
-@media (max-width: 540px) {
-  .header-nav, .header-nav-small {
-    width: 40vw;
-  }
-  .header-nav-small {
-    width: 20vw;
-  }
-  .page-content {
-    margin-left: 40vw;
-  }
-  .header-nav-small ~ .page-content {
-    margin-left: 20vw;
-  }
-}
-
-@media (max-width: 460px) {
-  .header-nav, .header-nav-small {
-    width: 50vw;
-  }
-  .header-nav-small {
-    width: 25vw;
-  }
-  .page-content {
-    margin-left: 50vw;
-  }
-  .header-nav-small ~ .page-content {
-    margin-left: 25vw;
-  }
-}
 </style>
