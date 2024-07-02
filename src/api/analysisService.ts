c5c3596b7e4fb3dd13f8c3c6076622e5421b3e93
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-07-02 11:33:22 -0300

    feat: cria e aplic tipo de análises com um chave result do tipo number

--- src/api/analysisService.ts
+++ src/api/analysisService.ts
@@ -1,10 +1,12 @@
 import { BaseService } from "./abstractService";
+import { type analysis } from "../types/analysis";
 
 class AnalysisService extends BaseService {
   async getAnalysisProductSalesByDayOfWeek(storeId: number, success: (data: any) => void) {
     const response = await this.getEntity(`/analysis/anacor?store_id=${storeId}`);
     if (response.ok) {
       const data = await response.json();
+      console.log(data);
       success(data);
     }
   }
@@ -13,11 +15,12 @@ class AnalysisService extends BaseService {
     const response = await this.getEntity(`/analysis/monthly_analysis?store_id=${storeId}`);
     if (response.ok) {
       const data = await response.json();
+      console.log(data);
       success(data);
     }
   }
 
-  async getTotalOrders(storeId: number, success: (data: any) => void) {
+  async getTotalOrders(storeId: number, success: (data: analysis) => void) {
     const response = await this.getEntity(`/analysis/total_orders?store_id=${storeId}`);
     if (response.ok) {
       const data = await response.json();
@@ -25,7 +28,7 @@ class AnalysisService extends BaseService {
     }
   }
 
-  async getTotalSales(storeId: number, success: (data: any) => void) {
+  async getTotalSales(storeId: number, success: (data: analysis) => void) {
     const response = await this.getEntity(`/analysis/total_sales?store_id=${storeId}`);
     if (response.ok) {
       const data = await response.json();
@@ -33,7 +36,7 @@ class AnalysisService extends BaseService {
     }
   }
 
-  async getPendingOrders(storeId: number, success: (data: any) => void) {
+  async getPendingOrders(storeId: number, success: (data: analysis) => void) {
     const response = await this.getEntity(`/analysis/pending_orders?store_id=${storeId}`);
     if (response.ok) {
       const data = await response.json();
---
+++ src/types/analysis.ts
@@ -0,0 +1,3 @@
+export type analysis = {
+    result: number
+}
